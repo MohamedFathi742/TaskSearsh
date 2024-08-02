dfdd1283d9082daae7b250f7346b5085e3e92be6
MohamedFathi742 <mofathi742002@gmail.com>
2024-08-02 23:14:52 +0300

    first commit

---
+++ StringSearch/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace StringSearch
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Plesse enter string");
+                string strings = Console.ReadLine();
+                if (strings != "i,o,u,e,a")
+
+                    throw new Exception("string does not contain vowels.");
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Error: {ex.Message}");
+
+            }
+            finally {
+                Console.WriteLine(" you're Finished");
+
+            }
+
+
+
+        }
+        }
+    }
+
---
+++ TaskSearsh/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace TaskSearsh
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Plesse enter list fo numbers separated by comma ");
+                string input = Console.ReadLine();
+
+                string[] numbers = input.Split(" , " );
+
+                int num = 0;
+                foreach (var item in numbers)
+                {
+                    num=int.Parse(item);
+                    //if ( num==  ) { 
+                    
+                    
+                    
+                    
+                    //}
+                    
+                        
+                    
+                }
+                throw new Exception("Plesse enter unduplicate numbers ");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error : {ex.Message}");
+
+            }
+
+            finally {
+
+                Console.WriteLine(" you're Finished");
+
+            }
+        }
+
+    }
+}
+
+
